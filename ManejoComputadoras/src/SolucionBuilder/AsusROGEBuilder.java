e5b03eb39415531e06fcfe50aa11447f5755fd61
Stefyft <lovestefa.ef@gmail.com>
2018-12-11 13:27:32 -0500

    Final

--- ManejoComputadoras/src/SolucionBuilder/AsusROGEBuilder.java
+++ ManejoComputadoras/src/SolucionBuilder/AsusROGEBuilder.java
@@ -16,12 +16,18 @@ public class AsusROGEBuilder extends ComputadoraBuilder{
 
 	@Override
 	public void construirSO() {
-		
+		SistemaOperativo so= new SistemaOperativo();
+		so.setNombre("Windows");
+		so.setVersion("10 PRO");
+				so.setArquitectura(64);
+				c.setSo(so);
 	}
 
 	@Override
 	public void construirMainboard() {
+		Mainboard mb= new Mainboard("Strix","x99");
 		
+		c.setPlaca(mb);
 	}
 
 	
--- ManejoComputadoras/src/SolucionBuilder/AsusZenbook.java
+++ ManejoComputadoras/src/SolucionBuilder/AsusZenbook.java
@@ -15,13 +15,20 @@ public class AsusZenbook extends ComputadoraBuilder{
 
 	@Override
 	public void construirSO() {
-		
+		SistemaOperativo so= new SistemaOperativo();
+		so.setNombre("Windows");
+		so.setVersion("10 HOME");
+				so.setArquitectura(64);
+				c.setSo(so);
 	}
 
 	@Override
 	public void construirMainboard() {
+Mainboard mb= new Mainboard("prime","z370");
 		
+		c.setPlaca(mb);
 	}
+	
 
 	
 }
